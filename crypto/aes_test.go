8c5797fd76669fd78058da0377e28713e4bd9839
liuql <liuql_cs@163.com>
2020-05-29 09:14:50 +0800

    fix[crypto]: 新增pkcs7填充逻辑

--- crypto/aes_test.go
+++ crypto/aes_test.go
@@ -80,7 +80,8 @@ func TestAesCbc(t *testing.T) {
 
 	text := []byte(`welcome`)
 
-	textPadding  := pkcs5Padding(text, aes.BlockSize)
+	textPadding, err  := pkcs7Padding(text, aes.BlockSize)
+	assert.Nil(t, err)
 
 	cipherText := make([]byte, len(textPadding))
 	encrypter := cipher.NewCBCEncrypter(block, iv)
@@ -89,23 +90,59 @@ func TestAesCbc(t *testing.T) {
 	newText := make([]byte, len(cipherText))
 	decrypter := cipher.NewCBCDecrypter(block, iv)
 	decrypter.CryptBlocks(newText, cipherText)
-	newText, err = pkcs5Unpadding(newText, aes.BlockSize)
+	newText, err = pkcs7Unpadding(newText, aes.BlockSize)
 	assert.Nil(t, err)
 	assert.Equal(t, text, newText)
 }
 
-func pkcs5Padding(ciphertext []byte, blockSize int) []byte {
-	padding := blockSize - len(ciphertext)%blockSize
-	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
-	return append(ciphertext, padtext...)
+
+func pkcs5Padding(plaintext []byte) []byte {
+	paddingLen := 8 - len(plaintext) % 8
+	paddingText := bytes.Repeat([]byte{byte(paddingLen)}, paddingLen)
+
+	return append(plaintext, paddingText...)
 }
 
-func pkcs5Unpadding(src []byte, blockSize int) ([]byte, error) {
-	length := len(src)
-	unpadding := int(src[length-1])
-	if unpadding >= length || unpadding > blockSize {
-		return nil, errors.New("there's error")
+func pkcs5Unpadding(ciphertext []byte) ([]byte, error) {
+	paddingLen := int(ciphertext[len(ciphertext)-1])
+
+	if paddingLen < 0 || paddingLen >= 8 {
+		return nil, errors.New("padding length should not larger than blockSize")
 	}
 
-	return src[:length-unpadding], nil
+	return ciphertext[:len(ciphertext)-paddingLen], nil
 }
+
+
+func pkcs7Padding(plaintext []byte, blockSize int) ([]byte, error) {
+	if blockSize <= 0 {
+		return nil, errors.New("invalid block size")
+	}
+
+	if plaintext == nil || len(plaintext) == 0 {
+		return nil, errors.New("invalid pkcs7 data")
+	}
+
+	n := blockSize - (len(plaintext)%blockSize)
+
+	return append(plaintext, bytes.Repeat([]byte{byte(n)}, n)...), nil
+}
+
+// pkcs stands for "Public Key Cryptography Standards"
+func pkcs7Unpadding(ciphertext []byte, blockSize int) ([]byte, error) {
+	if blockSize <= 0 {
+		return nil, errors.New("invalid block size")
+	}
+
+	if ciphertext == nil || len(ciphertext) == 0 {
+		return nil, errors.New("invalid pkcs7 data")
+	}
+
+	paddingLen := int(ciphertext[len(ciphertext)-1])
+	if paddingLen < 0 || paddingLen >= blockSize {
+		return nil, errors.New("invalid padding size")
+	}
+
+	return ciphertext[:len(ciphertext)-paddingLen], nil
+}
+
--- crypto/readme.md
+++ crypto/readme.md
@@ -6,4 +6,12 @@
 
 ## 如果第三方偷换 公钥给sender，怎么办？选择PKI
 
-## 
+## PKCS#5和PKCS7
+pkcs是""Public Key Cryptography Standards"的简称，pkcs#5是
+pkcs#7的一种特殊情况，即blockSize = 8byte，而pkcs7则可以是小于255bytes的任意自己；
+AES的block size是16bytes，所以AES采用的一定是pkcs7
+
+## AES CBC生成的密文和明文长度一样？
+一般stream cipher算法每bit或byte逐个进行加密，密文和明文长度保持一致；block cipher
+算法以blocksize为单位进行加密，加密后的长度比明文多padding个长度；
+
